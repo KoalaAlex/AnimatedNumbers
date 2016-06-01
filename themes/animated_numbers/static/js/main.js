5c348d708a64f7b79ddaa7b479e0dd7030876461
KoalaAlex <you@example.com>
2016-06-01 23:18:50 +0200

    changes after Crash

--- themes/animated_numbers/static/js/main.js
+++ themes/animated_numbers/static/js/main.js
@@ -1,18 +1,20 @@
-var em;
-var delta;
 var deltaY;
 var deltaX;
 var relativeY;
 var windowHeight;
 var windowWidth;
-var scale = 1;
+var orientationDevice = 0;
 
-var lastScrollY = 0,
-    ticking = false,
+//Debug
+var debugDevice = document.getElementById('debug_device');
+
+var heroOne = document.getElementById('hero1');
+var heroTwo = document.getElementById('hero2');
+
+var ticking = false,
     bgElmOne = document.getElementById('layer_one'),
     bgElm = document.getElementById('layer_two'),
-    headerText = document.getElementById('starter_text'),
-    speedDivider = -20;
+    headerText = document.getElementById('starter_text');
 
 // Y
 var translateValueY = -4;
@@ -27,7 +29,22 @@ var minTranslateX = -10;
 $(document).ready(function(){
   windowHeight = window.innerHeight;
   windowWidth = window.innerWidth;
-  document.onmousemove = requestTickMouse();
+  // document.onmousemove = requestTickMouse();
+  // If no tablet or smartphone
+  if(window.orientation == undefined){
+    heroOne.onmousemove = requestMouseHeroOne();
+    heroTwo.onmousemove = requestMouseHeroTwo();
+  }
+  else {
+    orientationDevice = window.orientation;
+    window.ondeviceorientation = requestDeviceHeroOne();
+    //window.ondeviceorientation = requestDeviceHeroTwo();
+    //window.addEventListener("ondeviceorientation", requestDeviceHeroOne(), false);
+    //window.addEventListener("ondeviceorientation", requestDeviceHeroTwo(), false);
+    window.addEventListener("orientationchange", function(){
+      orientationDevice = window.orientation;
+    }, false);
+  }
 
   // Gyroscope
   if (window.DeviceMotionEvent==undefined) {
@@ -43,20 +60,16 @@ $(document).ready(function(){
         arBeta = Math.round(rotation.beta);
         arGamma = Math.round(rotation.gamma);
       }
-      alert("Numebrs: " + ax + " " + ay + " " + az);
-    }
-
-    window.ondeviceorientation = function(event) {
-      alpha = Math.round(event.alpha);
-      beta = Math.round(event.beta);
-      gamma = Math.round(event.gamma);
     }
   }
 
   // First Translate
+  // hero1
   translateY3d(bgElm, translateValueX, translateValueY);
-
   translateY3d(headerText, translateValueX * 0.5, (translateValueY * 0.5) - 80);
+  // hero2
+  translateY3d(bgElm_h2, translateValueX_h2, translateValueY_h2);
+  translateY3d(headerText_h2, translateValueX_h2 * 0.5, (translateValueY_h2 * 0.5) - 80);
 
   return false;
 });
@@ -67,26 +80,14 @@ $(document).resize(function(){
   return false;
 });
 
-/*
-var requestTickWheel = function() {
+// Hero1
+var requestMouseHeroOne = function() {
   return function (e) {
     if (!ticking) {
       ticking = true;
-      // cross-browser wheel delta
-      em = window.event || e;
-      delta = Math.max(-1, Math.min(1, (em.wheelDelta || -em.detail)));
-      console.log(e.clientY);
-      window.requestAnimationFrame(checkScroll);
-    }
-    return false;
-  }
-};*/
-
-var requestTickMouse = function() {
-  return function (e) {
-    if (!ticking) {
-      ticking = true;
-      // cross-browser wheel delta
+      /*  alpha = Math.round(event.alpha);
+          beta = Math.round(event.beta);
+          gamma = Math.round(event.gamma); */
       deltaY = e.clientY / windowHeight;
       deltaX = e.clientX / windowWidth;
       window.requestAnimationFrame(checkMousePos);
@@ -95,7 +96,45 @@ var requestTickMouse = function() {
   }
 };
 
-//window.addEventListener('wheel', requestTickWheel(), false);
+function Clamp(value, min, max){
+  if(value < min){
+    value = min;
+  }
+  else if(value > max){
+    value = max;
+  }
+  return value;
+}
+
+var requestDeviceHeroOne = function() {
+  return function (e) {
+    if (!ticking) {
+      ticking = true;
+      // cross-browser wheel delta
+      if(orientationDevice === 0){
+        deltaY = Math.round(event.beta);
+        deltaX = Math.round(event.gamma);
+      }
+      else if(orientationDevice === 90){
+        deltaY = -Math.round(event.gamma);
+        deltaX = Math.round(event.beta);
+      } else {
+        deltaY = Math.round(event.gamma);
+        deltaX = -Math.round(event.beta);
+      }
+      deltaY = Clamp(deltaY, -20, 20);
+      deltaY = (deltaY+20) /40;
+      /*if(deltaX >= 180){
+        deltaX -= 360;
+      } */
+      deltaX = Clamp(deltaX, -20, 20);
+      deltaX = (deltaX+20) /40;
+      //debugDevice.innerHTML = deltaY + ", " + deltaX;
+      window.requestAnimationFrame(checkMousePos);
+    }
+    return false;
+  }
+};
 
 function checkMousePos(){
   translateValueY = -((deltaY) * (minTranslateY - maxTranslateY)) + minTranslateY;
@@ -126,3 +165,84 @@ var translateY3d = function(elm, valueX, valueY) {
   elm.style['-o-transform'] = translate;
   elm.style.transform = translate;
 };
+
+// Hero2
+var deltaY_h2;
+var deltaX_h2;
+var relativeY;
+
+var ticking_h2 = false,
+    bgElmOne_h2 = document.getElementById('h2_layer_one'),
+    bgElm_h2 = document.getElementById('h2_layer_two'),
+    headerText_h2 = document.getElementById('h2_starter_text');
+
+// Y
+var translateValueY_h2 = -4;
+var maxTranslateY_h2 = -20;
+var minTranslateY_h2 = -4;
+
+// X
+var translateValueX_h2 = 0;
+var maxTranslateX_h2 = 10;
+var minTranslateX_h2 = -10;
+
+var requestMouseHeroTwo = function() {
+  return function (e) {
+    if (!ticking_h2) {
+      ticking_h2 = true;
+      // cross-browser wheel delta
+      deltaY_h2 = e.clientY / windowHeight;
+      deltaX_h2 = e.clientX / windowWidth;
+      window.requestAnimationFrame(checkMousePosHeroTwo);
+    }
+    return false;
+  }
+};
+
+var requestDeviceHeroTwo = function() {
+  return function (e) {
+    if (!ticking_h2) {
+      ticking_h2 = true;
+      if(orientationDevice === 0){
+        deltaY_h2 = Math.round(event.beta);
+        deltaX_h2 = Math.round(event.gamma);
+      }
+      else if(orientationDevice === 90){
+        deltaY_h2 = -Math.round(event.gamma);
+        deltaX_h2 = Math.round(event.beta);
+      } else {
+        deltaY_h2 = Math.round(event.gamma);
+        deltaX_h2 = -Math.round(event.beta);
+      }
+      deltaY_h2 = Clamp(deltaY_h2, -20, 20);
+      deltaY_h2 = (deltaY_h2+20) /40;
+      /*if(deltaX >= 180){
+        deltaX -= 360;
+      } */
+      deltaX_h2 = Clamp(deltaX_h2, -20, 20);
+      deltaX_h2 = (deltaX_h2+20) /40;
+      //debugDevice.innerHTML = deltaY + ", " + deltaX;
+      window.requestAnimationFrame(checkMousePosHeroTwo);
+    }
+    return false;
+  }
+};
+
+function checkMousePosHeroTwo(){
+  translateValueY_h2 = -((deltaY_h2) * (minTranslateY_h2 - maxTranslateY_h2)) + minTranslateY_h2;
+  translateValueX_h2 = ((1 - deltaX_h2) * (maxTranslateX_h2 - minTranslateX_h2)) + minTranslateX_h2;
+
+  // We don't want parallax to happen if scrollpos is below 0
+
+  translateY3d(bgElm_h2, translateValueX_h2, translateValueY_h2);
+
+  translateY3d(headerText_h2, translateValueX_h2 * 0.5, (translateValueY_h2 * 0.5) - 80);
+
+  // Blur Smooth
+  // bgElmOne.style['-webkit-filter'] = 'blur(' + Math.floor((4 - (translateValue - minTranslate)/4)) + 'px)';
+  bgElmOne_h2.style['-webkit-filter'] = 'blur(' + (4 - (translateValueY_h2 - maxTranslateY_h2)/4) + 'px)';
+
+  // Stop ticking
+  ticking_h2 = false;
+  return false;
+}
